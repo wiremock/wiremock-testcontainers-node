872a459b5c12c4398ddc75f6df706da7086fc7d7
Aditya Ghidora <adityaghidora@gmail.com>
2023-10-13 22:02:21 +0530

    hello world test works now

--- wiremock_module_demo.js
+++ wiremock_module_demo.js
@@ -1,20 +1,21 @@
 import { GenericContainer } from "testcontainers";
-const mapping = {
-  request: {
-    method: "GET",
-    url: "/hello",
-  },
-
-  response: {
-    status: 200,
-    body: "Hello, world!",
-  },
-};
-
 const container = await new GenericContainer("wiremock/wiremock")
   .withExposedPorts(8080)
+  .withCopyFilesToContainer([
+    {
+      source: "./mappings/hello_world.json",
+      target: "/home/wiremock/mappings/hello_world.json",
+    },
+  ])
   .start();
 
+// const { output, exitCode } = await container.exec(["pwd"]);
+// const { output, exitCode } = await container.exec(["ls", "mappings", "-al"]);
+
+// const { output, exitCode } = await container.exec([
+//   "curl",
+//   "http://localhost:8080/__admin/mappings",
+// ]);
 const { output, exitCode } = await container.exec([
   "curl",
   "http://localhost:8080/hello",
